db0794a41942583315c2225af4f24b276a44d80c
Milanpal7889 <milanpaul212@gmail.com>
2023-08-10 11:28:08 +0530

    minor changes

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,12 +1,12 @@
-  const express = require('express');
-  const { body, validationResult } = require('express-validator');
-  const User = require('../models/User');
-  const bcrypt = require('bcryptjs');
-  const jwt = require('jsonwebtoken');
+  const express = require ('express');
+  const { body, validationResult } = require ('express-validator');
+  const User = require ('../models/User');
+  const bcrypt = require ('bcryptjs');
+  const jwt = require ('jsonwebtoken');
   const router = express.Router();
-  const fetchuser = require('../middleware/fetchuser')
+  const fetchuser = require ('../middleware/fetchuser')
   const JWT_SECRET = "a0f9c6d3b0f8c9a5f6b7c8d9e0f1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9";
-  const success = true
+  let success = true
   //Route 1: Create a user using: POST "/api/auth/create". doesn't require auth
 
   router.get('/new',(req, res)=>{
@@ -82,7 +82,7 @@
         }
       };
       const authToken = jwt.sign(data, JWT_SECRET);
-      res.json(success,authToken);
+      res.json(authToken);
     } catch (error) {
       success = false
       console.error(error.message);
